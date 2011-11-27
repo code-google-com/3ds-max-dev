e707f6609a7b1c18f56871cb69853bc7a055e80a
cdiggins@gmail.com <cdiggins@gmail.com@d8b7b0d6-0477-47ce-59ba-a56a25053fdc>
2011-11-27 03:00:17 +0000

    Assuring all the files are checked in.

--- sandbox/cdiggins/PythonLauncher/scripts/max_test.py
+++
@@ -1,6 +0,0 @@
-import autodesk
-
-app = autodesk.common.app
-app.write_line(autodesk.scene_json())
-if autodesk.selected():
-	app.write_line(autodesk.selected_mesh_json())
--- sandbox/cdiggins/PythonLauncher/scripts/mobu_demo.py
+++
@@ -1,5 +0,0 @@
-import init_platform
-init_platform.init_mobu()
-
-import client_demo
-g = client_demo.demo('http://localhost:8001')
